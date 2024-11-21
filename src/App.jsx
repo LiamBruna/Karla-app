353d6bed68165897150e32f4ffc88dddd4801f60
Ven0s00 <135740615+Ven0s00@users.noreply.github.com>
2024-11-21 02:04:41 -0300

    Deploy to Github Pages

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="glass">
+        <div className="shine"></div>
+      </div>
+
+      <div className="thorns">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      <div className="leaves">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      <div className="petals">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      <div className="deadPetals">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      <div>
+        <h4 className="texto">Mi amor por ti durará hasta que caiga el último pétalo ❤️</h4>
+      </div>
+    </div>
+  );
+}
+
+export default App;
